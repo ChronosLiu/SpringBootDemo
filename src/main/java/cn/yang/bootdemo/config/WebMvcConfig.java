9e2a7c8cd4a4e6cce032a2f26a6b3becbcc16689
liuyang <liuyang8218@163.com>
2018-03-01 18:41:13 +0800

    扩展MVC配置,自定义拦截器和静态资源映射

--- src/main/java/cn/yang/bootdemo/config/WebMvcConfig.java
+++ src/main/java/cn/yang/bootdemo/config/WebMvcConfig.java
@@ -1,20 +1,51 @@
 package cn.yang.bootdemo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
+ * 扩展MVC
  * @Author: liuyang
  * @Description:
  * @Date: Created in 9:23 2018/2/28
  */
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    /**
+     * 自定义页面跳转控制
+     * @param registry
+     */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         //注册访问/login转向login.html页面
         registry.addViewController("/login").setViewName("login");
-      //  super.addViewControllers(registry);
+        super.addViewControllers(registry);
+    }
+
+    /**
+     * 自定义静态资源映射
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //方式：addResourceHandler 指定映射url,addResourceLocation指定路径
+        //registry.addResourceHandler("/my/**").addResourceLocations("classpath:/my/");
+        super.addResourceHandlers(registry);
+    }
+
+    /**
+     * 自定义拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //addINterceptor添加自定义的拦截器 ，实现HandlerInterceptor 接口
+        //addPathPatterns 添加拦截规则
+        //execludePathPatterns 排除拦截，例外情况
+       // registry.addInterceptor(1).addPathPatterns("/**").excludePathPatterns("/toLogin","/login");
+        super.addInterceptors(registry);
     }
 }
